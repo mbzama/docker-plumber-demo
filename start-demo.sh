ff04e5096b522d2492f92a9d625dd53560ce17fc
mbzama <zamamb@gmail.com>
2019-03-07 16:18:59 +0530

    Updated image

--- start-demo.sh
+++ start-demo.sh
@@ -1,4 +1,4 @@
 #To run directly: Rscript /apps/demo-api.R
 
 docker rm -f plumber-demo
-docker run --name plumber-demo -d -p 8008:8008 docker-plumber-demo 
+docker run --name plumber-demo -d -p 8008:8008 zamamb/docker-plumber-demo 
