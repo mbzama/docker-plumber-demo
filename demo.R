b89b261d0a2a708449f77e4ee5092a762b30e1d7
mbzama <zamamb@gmail.com>
2019-03-08 17:03:35 +0530

    Fix merge issue

--- demo.R
+++ demo.R
@@ -1,12 +1,7 @@
 
 #* @get /version
-<<<<<<< HEAD
 normalMean <- function(samples=10){
   "Version: 1.1"
-=======
-version <- function(samples=10){
-  "Version: 1.0"
->>>>>>> 9d054d553b1b34fe23148a7fd3a64a8e87a02543
 }
 
 #* @get /heartbeat
